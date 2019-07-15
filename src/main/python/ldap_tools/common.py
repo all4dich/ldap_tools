e65b30e82cc26d00e8bea263c424be344bfe520b
Sunjoo Park <all4dich@gmail.com>
2019-07-15 14:08:15 +0900

    Get all members from a department
    If a user calls LDAPCliet.get_mebers with a group name,
    A caller will get all people under that group.
    
    But a caller have to set 'self._search_root' before  calling
    a method

--- src/main/python/ldap_tools/common.py
+++ src/main/python/ldap_tools/common.py
@@ -51,12 +51,33 @@ class LDAPClient:
         conn.bind()
         return conn
 
+    def get_departments(self, dept_name):
+        conn = self.get_connection()
+        if self._search_root is None:
+            logger.warning("Set 'search_root' and try again")
+            return None
+        conn.search(self._search_root, '(cn=' + dept_name +')',
+                     attributes=[
+                         'cn', 'mail', 'displayName',
+                         'mobile', 'description',
+                     ]
+                     )
+        return conn.entries
+
     def get_members(self, dept_name):
+        members = []
         conn  = self.get_connection()
         if self._search_root is None:
             logger.warning("Set 'search_root' and try again")
             return None
-        conn.search(self._search_root, '(objectClass=person)',
-                    attributes=['name', 'mail', 'mobile', 'cn', 'department', 'description','displayNamePrintable', 'displayName'])
-        return conn.entries
+        #Get a dapartment object
+        depts = self.get_departments(dept_name)
+        for each_dept in depts:
+            member_search_base = each_dept.entry_dn.split(",")[1:]
+            conn.search(member_search_base, '(objectClass=person)',
+                        attributes=['name', 'mail', 'mobile', 'cn', 'department', 'description',
+                                    'displayNamePrintable', 'displayName'])
+            for each_entry in conn.entries:
+                members.append(each_entry)
+        return members
 
