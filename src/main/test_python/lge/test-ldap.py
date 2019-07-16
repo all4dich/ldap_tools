4a681c4636379e5375a0555cbd528df752021c34
Sunjoo Park <all4dich@gmail.com>
2019-07-16 10:25:02 +0900

    Update test codes

--- src/main/test_python/lge/test-ldap.py
+++ src/main/test_python/lge/test-ldap.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     a = LDAPClient("156.147.162.251", "\\addhost", "1qaz2wsx")
     a.search_attributes.append("distinguishedName")
     a.search_root = 'ou=LGE Users, dc=lge, dc=net'
-    r = a.get_members("TV선행SW*_grp")
+    r = a.get_members("TV LCM High UHD Project")
     members_info = list(map(lambda x: x.entry_dn, r))
-#    print("\n".join(members_info))
+    print("\n".join(members_info))
 
     r2 = a.get_objects(f"&(objectClass=organizationalUnit)(ou=*TV선행SW*)")
     print(r2[0].entry_dn)
