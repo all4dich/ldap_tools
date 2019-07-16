d6df4d0f126686029b704378fadb007f93f09fb0
Sunjoo Park <all4dich@gmail.com>
2019-07-16 10:06:35 +0900

    Update test codes for LGE internal use only

---
+++ src/main/test_python/lge/test-ldap.py
@@ -0,0 +1,16 @@
+from ldap_tools.common import LDAPClient
+
+if __name__ == "__main__":
+    a = LDAPClient("156.147.162.251", "\\addhost", "1qaz2wsx")
+    a.search_attributes.append("distinguishedName")
+    a.search_root = 'ou=LGE Users, dc=lge, dc=net'
+    r = a.get_members("TV선행SW*_grp")
+    members_info = list(map(lambda x: x.entry_dn, r))
+#    print("\n".join(members_info))
+
+    r2 = a.get_objects(f"&(objectClass=organizationalUnit)(ou=*TV선행SW*)")
+    print(r2[0].entry_dn)
+    a.search_root = r2[0].entry_dn
+    r3 = a.get_child(oc="person")
+    for each_member in r3:
+        print(each_member.entry_dn)
