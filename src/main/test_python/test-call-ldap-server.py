c6687dcbf1a0dae25e4219b8ebfbc5cc7c8a20d9
Sunjoo Park <all4dich@gmail.com>
2019-07-16 10:05:15 +0900

    Update test codes

--- src/main/test_python/test-call-ldap-server.py
+++ src/main/test_python/test-call-ldap-server.py
@@ -7,8 +7,14 @@ if __name__ == "__main__":
 
     a = LDAPClient(host,username,password)
     a.search_root = 'dc=tv, dc=sunjoo, dc=org'
-    b = a.get_child(oc="posixGroup")
-    members = list(map(lambda x: x.entry_dn, b))
-    print("\n".join(members))
-    c = a.get_objects("cn=miners")
-    print(c)
+#    b = a.get_child(oc="posixGroup")
+#    print(b)
+#    members = list(map(lambda x: x.entry_dn, b))
+#    print("\n".join(members))
+#    c = a.get_objects("ou=TV SW Engineering")
+#    print(c)
+    d = a.get_child(oc="person")
+    for each_entry in d:
+        print(each_entry.entry_dn)
+    d = a.get_objects("&(objectClass=person)(gidNumber=5001)")
+    print(d)
