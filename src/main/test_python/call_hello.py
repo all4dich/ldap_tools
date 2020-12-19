f8ae759231e92b7791397c1f2f3169c3badc1191
Sunjoo Park <all4dich@gmail.com>
2020-12-20 01:31:48 +0900

    test: Update sample codes

--- src/main/test_python/call_hello.py
+++ src/main/test_python/call_hello.py
@@ -1,3 +1,25 @@
-from atlassian import hello
+import re
+import os
+import logging
 
-hello.world()
+from ldap_tools.common import LDAPClient
+
+logger = logging.getLogger("add-lge-users-to-nexus")
+
+if __name__ == "__main__":
+    host = "156.147.162.251"
+    username = "lge\\allessunjoo.park"
+    password = input("Password: ")
+
+    a = LDAPClient(host, username, password)
+    a.search_root = 'ou=LGE Users,dc=lge,dc=net'
+    try:
+        lge_department_name = "TV DevOps개발"
+    except:
+        lge_department_name = input("LGE department name: ")
+    r = a.get_members(lge_department_name)
+    for member in r:
+        user_cn = str(member.cn)
+        user_mail = str(member.mail)
+        print(user_mail)
+
--- src/main/test_python/test-call-ldap-server.py
+++ src/main/test_python/test-call-ldap-server.py
@@ -1,20 +1,24 @@
 from ldap_tools.common import LDAPClient
-
+import subprocess
+import re
 if __name__ == "__main__":
-    host = "192.168.57.4"
-    username = "\\admin"
-    password = "admin"
+    host = "156.147.162.251"
+    username = "lge\\addhost"
+    password = input(f"Password for {username}")
 
     a = LDAPClient(host,username,password)
-    a.search_root = 'dc=tv, dc=sunjoo, dc=org'
+    a.search_root = 'ou=LGE Users,dc=lge,dc=net'
 #    b = a.get_child(oc="posixGroup")
 #    print(b)
 #    members = list(map(lambda x: x.entry_dn, b))
 #    print("\n".join(members))
 #    c = a.get_objects("ou=TV SW Engineering")
 #    print(c)
-    d = a.get_child(oc="person")
-    for each_entry in d:
-        print(each_entry.entry_dn)
-    d = a.get_objects("&(objectClass=person)(gidNumber=5001)")
-    print(d)
+#    user = "jaewooki.lee"
+#    d = a.get_objects(f"sAMAccountName=*{user}*")
+#    for e in d:
+#        print(e)
+    team = "TV SW CI팀"
+    r = a.get_members(team)
+    for member in r:
+        print(member.cn)
